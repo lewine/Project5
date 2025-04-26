ebcb5196be94e623effc1b747e6842979e970355
Chris-a-s <christianschroeder750@gmail.com>
2025-04-26 09:40:38 -0700

    I couldn't figure out the captcha
    I don't know how needed it is, but I added a simple captcha I will edit later but Cyler wanted this pushed so here. Also note that I can redirect to another page besides default with an easy line so I am just pushing what I got so far

---
+++ Member.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Member.aspx.cs" Inherits="Assignment5_CSE445_Group_62.Member" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h2>Member Login</h2>
+        <asp:Label ID ="message" runat="server" ForeColor="Purple" />
+        <p>
+            Username: <asp:TextBox ID="txtUser" runat="server" />
+        </p>
+        <p>
+            Password: <asp:TextBox ID="txtPass" runat="server" />
+        </p>
+        <p>
+            CAPTCHA - TEMP: What is 2+2?
+            <asp:TextBox ID="captcha" runat="server" />
+        </p>
+        <p>
+            <asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click" />
+            <asp:Button ID="btnRegister" runat="server" Text="Register" OnClick="btnRegister_Click" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ Member.aspx.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.IO;
+
+namespace Assignment5_CSE445_Group_62
+{
+    public partial class Member : System.Web.UI.Page
+    {
+        string xmlPath = HttpContext.Current.Server.MapPath("~/App_Data/Member.xml");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+            if (captcha.Text.Trim() != "4")
+            {
+                message.Text = "Dumbass. Go get your smarts up before hooping";
+                return;
+            }
+            string user = txtUser.Text.Trim();
+            string password = txtPass.Text.Trim();
+            string hashword = CryptoUtils.HashString(password);
+
+            XmlDocument doc = new XmlDocument();
+            if (File.Exists(xmlPath))
+            {
+                doc.Load(xmlPath);
+            }
+            else
+            {
+                XmlElement root = doc.CreateElement("Members");
+                doc.AppendChild(root);
+            }
+
+            XmlElement newUser = doc.CreateElement("Member");
+            newUser.SetAttribute("username", user);
+            newUser.SetAttribute("password", hashword);
+
+            doc.DocumentElement.AppendChild(newUser);
+            doc.Save(xmlPath);
+
+            message.Text = "Success! Registration Complete";
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            string user = txtUser.Text.Trim();
+            string password = txtPass.Text.Trim();
+            string hashword = CryptoUtils.HashString(password);
+
+            XmlDocument doc = new XmlDocument();
+            if (!File.Exists(xmlPath))
+            {
+                message.Text = "We ain't got any userz";
+                return;
+            }
+
+            doc.Load(xmlPath);
+            XmlNodeList members = doc.SelectNodes("/Members/Member");
+            bool found = false;
+
+            foreach (XmlNode member in members)
+            {
+                var uss = member.Attributes["username"];
+                var pss = member.Attributes["password"];
+
+                if(uss != null && pss != null)
+                {
+                    if(uss.Value == user && pss.Value == hashword)
+                    {
+                        found = true;
+                        Session["username"] = user;
+                        Session["role"] = "member";
+                        Response.Redirect("Default.aspx");
+                        return;
+                    }
+                }
+            }
+            if (!found)
+            {
+                message.Text = "Username or Password Invalid.";
+            }
+
+        }
+
+    }
+}
